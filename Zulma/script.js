f4ad8546a4d5a2643170411faa7297f5c085bcf3
yamidferneymorabenavides-commits <yamidferneymorabenavides@gmail.com>
2026-05-24 16:31:25 -0500

    Carrusel

--- Zulma/script.js
+++ Zulma/script.js
@@ -1,3 +1,7 @@
+/* =========================
+   MODAL DE IMÁGENES
+========================= */
+
 const imagenes = document.querySelectorAll(".galeria img");
 const modal = document.getElementById("modal");
 const imagenGrande = document.getElementById("imagenGrande");
@@ -14,8 +18,40 @@ imagenes.forEach(imagen => {
 
 });
 
+/* CERRAR MODAL */
+
 cerrar.addEventListener("click", () => {
 
     modal.style.display = "none";
 
 });
+
+/* CERRAR AL DAR CLICK AFUERA */
+
+modal.addEventListener("click", (e) => {
+
+    if(e.target === modal){
+
+        modal.style.display = "none";
+
+    }
+
+});
+
+/* =========================
+   CARRUSEL ENCABEZADO
+========================= */
+
+const slides = document.querySelectorAll('.carrusel-header img');
+
+let index = 0;
+
+setInterval(() => {
+
+    slides[index].classList.remove('active');
+
+    index = (index + 1) % slides.length;
+
+    slides[index].classList.add('active');
+
+}, 4000);
